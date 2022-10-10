31c8f874813baf770ea9811c76b7e7ebaa28c6ed
BKB <blainekb94@gmail.com>
2022-10-10 01:36:45 -0500

    added in express middleware

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
+// express middleware
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use(express.static('public'));
 
 
 app.listen(PORT, () => {
